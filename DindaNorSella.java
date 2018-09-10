7e56b9ca74a78b81e793fdecc02a6a453e94bbec
Dindasella <43130776+Dindasella@users.noreply.github.com>
2018-09-10 14:25:13 +0700

    Add files via upload

---
+++ DindaNorSella.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dindanorsella;
+
+/**
+ *
+ * @author Dinda Sella
+ */
+public class DindaNorSella {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Tugas 1
+        int n=4;
+        for (int i=0; i<n; i++){
+            for (int d=0; d<=i; d++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
